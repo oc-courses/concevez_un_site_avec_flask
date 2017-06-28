6d3fff3ea0053a7a70fb659b1699d6357b7e76b9
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-06-28 15:34:43 +0200

    templating on progress / before inheritance

--- fbapp/views.py
+++ fbapp/views.py
@@ -10,3 +10,7 @@ app.config.from_object('config')
 @app.route('/index/')
 def index():
     return render_template('index.html')
+
+@app.route('/result/')
+def result():
+    return render_template('result.html')
