184f192ce040f22f0ef240c92fb0976870ff6cf4
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-06-29 16:01:26 +0200

    refacto

--- fbapp/utils.py
+++ fbapp/utils.py
@@ -4,6 +4,4 @@ from fbapp.models import Content
 
 def find_content(gender):
     contents = Content.query.filter(Content.gender == Content.GENDERS[gender]).all()
-    ids = [content.id for content in contents]
-    the_one = Content.query.get(random.choice(ids))
-    return the_one
+    return random.choice(contents)
