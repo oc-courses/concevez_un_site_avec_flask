d41732577dcade5d49cd97f4a65a6c7f4878bd44
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-07-03 17:12:52 +0200

    preproduction

--- config.py
+++ config.py
@@ -4,5 +4,8 @@ SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
 FB_APP_ID = 1200420960103822
 
 # Database initialization
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
