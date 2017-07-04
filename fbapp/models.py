752bf057430c6ff8d868c3a51feed7d6db58782e
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-07-04 17:38:54 +0200

    no enum for the moment

--- fbapp/models.py
+++ fbapp/models.py
@@ -1,20 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
-import enum
 
 from .views import app
 # Create database connection object
 db = SQLAlchemy(app)
 
-class Gender(enum.Enum):
-    female = 1
-    male = 2
-    other = 3
-
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Enum(Gender), nullable=False)
+    gender = db.Column(db.Integer(), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
@@ -23,7 +17,7 @@ class Content(db.Model):
 def init_db():
     db.drop_all()
     db.create_all()
-    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
-    db.session.add(Content("What's your favorite scary movie?", Gender['female']))
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", 1))
+    db.session.add(Content("What's your favorite scary movie?", 0))
     db.session.commit()
     lg.warning('Database initialized!')
