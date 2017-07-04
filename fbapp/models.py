a87d72a608b6df10ef824520ffbec9641944135a
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-07-04 17:46:16 +0200

    enusm

--- fbapp/models.py
+++ fbapp/models.py
@@ -1,22 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
+import enum
 
 from .views import app
 # Create database connection object
 db = SQLAlchemy(app)
 
+class Gender(enum.Enum):
+    female = 0
+    male = 1
+    other = 2
+
 class Content(db.Model):
-    GENDER_FEMALE = 'female'
-    GENDER_MALE = 'male'
-    GENDER_OTHER = 'other'
-    GENDERS = {
-        GENDER_FEMALE: 0,
-        GENDER_MALE: 1,
-        GENDER_OTHER: 2
-    }
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(200))
-    gender = db.Column('gender', db.Integer, nullable=False, default=GENDERS[GENDER_FEMALE])
+    description = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
@@ -25,6 +23,7 @@ class Content(db.Model):
 def init_db():
     db.drop_all()
     db.create_all()
-    db.session.add(Content("What's your favorite scary movie?", Content.GENDERS[Content.GENDER_MALE]))
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
+    db.session.add(Content("What's your favorite scary movie?", Gender['female']))
     db.session.commit()
     lg.warning('Database initialized!')
